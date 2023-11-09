12a3fd4975869d1a6654cbe32c63478102e2cf6c
Brian Shimkus <brianshimkus1@gmail.com>
2023-11-09 14:06:54 -0600

    Connected to MongoDB

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,7 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export const PORT = 5555
+
+export const mongoDBURL = process.env.MONGO_URI
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,23 @@
 import express from 'express'
-import { PORT } from './config.js'
+import { PORT, mongoDBURL } from './config.js'
+import mongoose from 'mongoose'
 
 const app = express()
+
+app.get('/', (req, res) => {
+	console.log(req)
+	return res.status(234).send('MERN')
+})
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
+
+mongoose
+	.connect(mongoDBURL)
+	.then(() => {
+		console.log('Connected to MongoDB')
+	})
+	.catch((err) => {
+		console.log('Error connecting to MongoDB: ', err.message)
+	})
